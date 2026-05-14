7deecb6ea95b33ca57169cbed424f04612ba621e
edcleyssonalves <edcleyssonalves@gmail.com>
2026-05-14 04:58:41 -0300

    estudo sobre a classe ChatPromptTemplate aplicando em um chat

---
+++ langchain-chat-prompt.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain_core.messages import SystemMessage, AIMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+
+
+load_dotenv()
+
+os.environ['OPENAI_API_KEY']
+
+model = ChatOpenAI(
+    model='gpt-3.5-turbo',
+    max_completion_tokens=500,
+
+)
+
+chat_template = ChatPromptTemplate.from_messages(  # base de um chatbot com inteligência artificial
+    [
+        SystemMessage(content='Você deve responder baseado em dados geográficos de regiões do Brasil.'),
+        HumanMessagePromptTemplate.from_template('Por favor, me fale sobre a região {regiao}.'),
+        AIMessage(content='Claro, vou começar coletando informações sobre a região e analisando os dados disponíveis.'),
+        HumanMessage(content='Certifique-se de incluir dados demográficos.'),
+        AIMessage(content='Entendido. Aqui estão os dados:'),
+    ]
+)
+
+prompt = chat_template.format_messages(regiao='Sul')
+
+response = model.invoke(prompt)
+
+print(response.content)
