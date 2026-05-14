56dae10948fede8ccff3451fcb405f094eedaf48
edcleyssonalves <edcleyssonalves@gmail.com>
2026-05-14 04:32:10 -0300

    atualiza langchain-openai.py

--- langchain-openai.py
+++ langchain-openai.py
@@ -1,21 +1,11 @@
 import os
-from langchain_openai import OpenAI, ChatOpenAI
 from dotenv import load_dotenv
+from langchain_openai import OpenAI, ChatOpenAI
 
 
 load_dotenv()
 
 os.environ['OPENAI_API_KEY']
-
-# model = OpenAI()
-
-# response = model.invoke(
-#     input='Quem foi Linux Torvalds',
-#     temperature=0.7,
-#     max_tokens=500,
-# )
-
-# print(response)
 
 model = ChatOpenAI(
     model='gpt-3.5-turbo',
